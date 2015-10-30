a317402508501ca737c14d03527329baf54f8a97
Rodrigo Perazzo <rodrigo.perazzo@gmail.com>
2015-10-30 09:39:42 -0300

    Try to identify user based on repo origin URL
    If no user is passed to "git user" command, the script will try
    to identify a suitable one based on repo origin URL. Additionally,
    if a user is informed but it not matches with origin URL, ask for
    saving it into ~/.gitusers file.

--- git-user.sh
+++ git-user.sh
@@ -1,34 +1,61 @@
 #!/bin/bash
 
 USERS_DB="$HOME/.gitusers"
-if [ ! -f $USERS_DB ]
+if [ ! -f "$USERS_DB" ]
 then
     touch $USERS_DB
 fi
+
+ask_for_update=false
+origin=$(git remote -v | grep -e "origin.*(push)" | sed 's/.*@//g' | sed 's/:.*//g')
+origin_info=$(grep -i "$origin" "$USERS_DB")
 
 # if no user profile was provided
 # try to identify one based on origin url
 if [ -z $1 ]
 then
-    origin=$(git remote -v | grep -e "origin.*(push)" | sed 's/.*@//g' | sed 's/:.*//g')
-    profile_info=$(grep -i $origin $USERS_DB)
+    profile_info=$origin_info
 else
     profile=$1
-    profile_info=$(grep -i $profile $USERS_DB)
+    profile_info=$(grep -i "$profile" "$USERS_DB")
+    if [ "$origin" != "$profile" ]
+    then
+        ask_for_update=true
+    fi
 fi
 
-if [ ! -z profile_info ]
+if [ ! -z "$profile_info" ]
 then
-    name=$(echo $profile_info | awk '{print $2}')
-    email=$(echo $profile_info | awk '{print $3}')
+    name=$(echo "$profile_info" | cut -d '"' -f2)
+    email=$(echo "$profile_info" | cut -d '"' -f3)
 
-    if [ ! -z $email ] && [ ! -z $name ]
+    if [ ! -z "$email" ] && [ ! -z "$name" ]
     then
+        # TODO
+        # git config user.name $name
         # git config user.email $email
         echo $name
         echo $email
+
+        if [ "$ask_for_update" == "true" ] && [ -z $(echo -e "${origin_info}" | tr -d '[[:space:]]') ]
+        then
+            echo "Save this user for $origin projects (y/n)?"
+            read -n 1 action
+
+            case "$action" in
+              y | yes )
+                yes=true;;
+              * )
+                ;;
+            esac
+            if [ "$yes" == "true" ]
+            then
+                echo "$origin \"$name\" $email" >> $USERS_DB
+            fi
+            echo ''
+        fi
     fi
 else
-    echo "Didn't find any user based on origin url or profile provided"
-    exit
+    echo "ERROR: User not found!"
+    exit 2
 fi
